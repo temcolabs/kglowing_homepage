e6a16cec294e17e96ca3342bd65e90087b1a7d2f
byunghalee <byungha.lee@temco.io>
2026-09-10 18:19:20 +0900

    TECH-23370 Update: clients logo update

--- src/components/sections/Clients.tsx
+++ src/components/sections/Clients.tsx
@@ -3,45 +3,40 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
+/** w, h: 2x PNG 픽셀 크기 (렌더 시 1/2) */
 const clients = [
-  { name: "belif", logo: "/images/clients/v2/brand-logo-1_v2.png", w: 145, h: 48 },
-  { name: "Sooryehan", logo: "/images/clients/v2/brand-logo-2_v2.png", w: 277, h: 72 },
-  { name: "CNP Laboratory", logo: "/images/clients/v2/brand-logo-3_v2.png", w: 318, h: 39 },
-  { name: "EUTHYMOL", logo: "/images/clients/v2/brand-logo-4_v2.png", w: 216, h: 42 },
-  { name: "ISA KNOX", logo: "/images/clients/v2/brand-logo-5_v2.png", w: 238, h: 39 },
-  { name: "Centellian 24", logo: "/images/clients/v2/brand-logo-6_v2.png", w: 279, h: 48 },
-  { name: "Lotte Wellfood", logo: "/images/clients/v2/brand-logo-7_v2.png", w: 305, h: 84 },
-  { name: "THOME", logo: "/images/clients/v2/brand-logo-8_v2.png", w: 189, h: 36 },
-  { name: "LADOR", logo: "/images/clients/v2/brand-logo-9_v2.png", w: 183, h: 39 },
-  { name: "MODA MODA", logo: "/images/clients/v2/brand-logo-10_v2.png", w: 156, h: 84 },
-  { name: "GD11", logo: "/images/clients/v2/brand-logo-11_v2.png", w: 138, h: 48 },
-  { name: "Glampalm", logo: "/images/clients/v2/brand-logo-12_v2.png", w: 264, h: 45 },
-  { name: "CLABIANE", logo: "/images/clients/v2/brand-logo-13_v2.png", w: 248, h: 36 },
-  { name: "EDML", logo: "/images/clients/v2/brand-logo-14_v2.png", w: 213, h: 51 },
-  { name: "FAU", logo: "/images/clients/v2/brand-logo-15_v2.png", w: 117, h: 51 },
-  { name: "Taesi.K", logo: "/images/clients/v2/brand-logo-16_v2.png", w: 192, h: 42 },
-  { name: "EXOPROXYL", logo: "/images/clients/v2/brand-logo-17_v2.png", w: 261, h: 36 },
-  { name: "pure:K", logo: "/images/clients/v2/brand-logo-18_v2.png", w: 198, h: 54 },
-  { name: "secret Key", logo: "/images/clients/v2/brand-logo-19_v2.png", w: 236, h: 60 },
-  { name: "RISKY", logo: "/images/clients/v2/brand-logo-20_v2.png", w: 141, h: 42 },
-  { name: "ROATTI", logo: "/images/clients/v2/brand-logo-21_v2.png", w: 183, h: 42 },
-  { name: "let's kared", logo: "/images/clients/v2/brand-logo-22_v2.png", w: 132, h: 78 },
-  { name: "BIOCESS", logo: "/images/clients/v2/brand-logo-23_v2.png", w: 225, h: 36 },
-  { name: "FEEV", logo: "/images/clients/v2/brand-logo-24_v2.png", w: 172, h: 33 },
-  { name: "OLLOCDAM", logo: "/images/clients/v2/brand-logo-25_v2.png", w: 252, h: 45 },
-  { name: "ROOTONIX", logo: "/images/clients/v2/brand-logo-26_v2.png", w: 248, h: 33 },
-  { name: "Olhaeyoung", logo: "/images/clients/v2/brand-logo-27_v2.png", w: 144, h: 144 },
-  { name: "anok", logo: "/images/clients/v2/brand-logo-28_v2.png", w: 153, h: 54 },
-  { name: "SERUMKIND", logo: "/images/clients/v2/brand-logo-29_v2.png", w: 336, h: 42 },
-  { name: "LALARECIPE", logo: "/images/clients/v2/brand-logo-30_v2.png", w: 288, h: 33 },
-  { name: "RAWDIVE", logo: "/images/clients/v2/brand-logo-31_v2.png", w: 225, h: 36 },
-  { name: "UmbraPhil", logo: "/images/clients/v2/brand-logo-32_v2.png", w: 250, h: 51 },
-  { name: "CLERIVY", logo: "/images/clients/v2/brand-logo-33_v2.png", w: 243, h: 39 },
-  { name: "ilso", logo: "/images/clients/v2/brand-logo-34_v2.png", w: 111, h: 54 },
-  { name: "LABO-H", logo: "/images/clients/v2/brand-logo-35_v2.png", w: 201, h: 36 },
-  { name: "lilyeve", logo: "/images/clients/v2/brand-logo-36_v2.png", w: 180, h: 57 },
-  { name: "roobliss", logo: "/images/clients/v2/brand-logo-37_v2.png", w: 198, h: 42 },
-  { name: "TITAD", logo: "/images/clients/v2/brand-logo-38_v2.png", w: 138, h: 33 },
+  { name: "CNP Laboratory", logo: "/images/clients/v3/brand-logo-1_v3.png", w: 212, h: 26 },
+  { name: "Sooryehan", logo: "/images/clients/v3/brand-logo-2_v3.png", w: 184, h: 48 },
+  { name: "belif", logo: "/images/clients/v3/brand-logo-3_v3.png", w: 96, h: 32 },
+  { name: "ISA KNOX", logo: "/images/clients/v3/brand-logo-4_v3.png", w: 158, h: 26 },
+  { name: "THOME", logo: "/images/clients/v3/brand-logo-5_v3.png", w: 136, h: 26 },
+  { name: "LADOR", logo: "/images/clients/v3/brand-logo-6_v3.png", w: 122, h: 26 },
+  { name: "Glampalm", logo: "/images/clients/v3/brand-logo-7_v3.png", w: 176, h: 30 },
+  { name: "Taesi.K", logo: "/images/clients/v3/brand-logo-8_v3.png", w: 128, h: 28 },
+  { name: "EXOPROXYL", logo: "/images/clients/v3/brand-logo-9_v3.png", w: 174, h: 24 },
+  { name: "secret Key", logo: "/images/clients/v3/brand-logo-10_v3.png", w: 158, h: 40 },
+  { name: "RISKY", logo: "/images/clients/v3/brand-logo-11_v3.png", w: 94, h: 28 },
+  { name: "let's kared", logo: "/images/clients/v3/brand-logo-12_v3.png", w: 88, h: 52 },
+  { name: "ROATTI", logo: "/images/clients/v3/brand-logo-13_v3.png", w: 122, h: 28 },
+  { name: "EDML", logo: "/images/clients/v3/brand-logo-14_v3.png", w: 142, h: 34 },
+  { name: "FAU", logo: "/images/clients/v3/brand-logo-15_v3.png", w: 72, h: 32 },
+  { name: "동해형제", logo: "/images/clients/v3/brand-logo-16_v3.png", w: 96, h: 96 },
+  { name: "FEEV", logo: "/images/clients/v3/brand-logo-17_v3.png", w: 114, h: 22 },
+  { name: "ROOTONIX", logo: "/images/clients/v3/brand-logo-18_v3.png", w: 165, h: 22 },
+  { name: "Huxley", logo: "/images/clients/v3/brand-logo-19_v3.png", w: 104, h: 40 },
+  { name: "UmbraPhil", logo: "/images/clients/v3/brand-logo-20_v3.png", w: 166, h: 34 },
+  { name: "CLABIANE", logo: "/images/clients/v3/brand-logo-21_v3.png", w: 166, h: 24 },
+  { name: "hersteller", logo: "/images/clients/v3/brand-logo-22_v3.png", w: 180, h: 34 },
+  { name: "SHURINK HOME", logo: "/images/clients/v3/brand-logo-23_v3.png", w: 180, h: 48 },
+  { name: "alternative stereo", logo: "/images/clients/v3/brand-logo-24_v3.png", w: 178, h: 44 },
+  { name: "CELLTRION", logo: "/images/clients/v3/brand-logo-25_v3.png", w: 180, h: 46 },
+  { name: "IUNIK", logo: "/images/clients/v3/brand-logo-26_v3.png", w: 140, h: 68 },
+  { name: "ilso", logo: "/images/clients/v3/brand-logo-27_v3.png", w: 74, h: 36 },
+  { name: "lilyeve", logo: "/images/clients/v3/brand-logo-28_v3.png", w: 128, h: 40 },
+  { name: "OSULLOC", logo: "/images/clients/v3/brand-logo-29_v3.png", w: 180, h: 26 },
+  { name: "LABO-H", logo: "/images/clients/v3/brand-logo-30_v3.png", w: 134, h: 24 },
+  { name: "mise en scène", logo: "/images/clients/v3/brand-logo-31_v3.png", w: 180, h: 32 },
+  { name: "ILLIYOON", logo: "/images/clients/v3/brand-logo-32_v3.png", w: 160, h: 24 },
 ];
 
 /** 브레이크포인트별 교차 행 패턴: [짧은행, 긴행] */
@@ -88,8 +83,8 @@ export default function Clients() {
               <Image
                 src={client.logo}
                 alt={client.name}
-                width={Math.round(client.w / 3)}
-                height={Math.round(client.h / 3)}
+                width={Math.round(client.w / 2)}
+                height={Math.round(client.h / 2)}
                 className="object-contain max-w-full max-h-full"
               />
             </div>
